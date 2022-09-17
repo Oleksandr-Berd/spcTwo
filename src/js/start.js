bb73a5799bc8132ab9251873f4dfcd3c3d14f933
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-09-17 22:11:39 +0300

    1

--- src/js/start.js
+++ src/js/start.js
@@ -2,6 +2,7 @@ const refs = {
   btnStart: document.querySelector('.btn-start'),
   contStart: document.querySelector('.cont-start'),
   btnLevelFirstPicture: '',
+  btnMovieFirst: '',
 };
 
 refs.btnStart.textContent = 'Let`s go Guys!';
@@ -11,14 +12,15 @@ refs.btnStart.addEventListener('click', renderSearchFormLevelFirst);
 function renderSearchFormLevelFirst() {
   const markUpSearchForm = `<div class="level-first">
   <button id="btnLvlFirstPct" class="btn__first" type='submit'>What picture are you looking for?</button>
-         <button type='submit'>Let's watch some movie!</button>
+         <button class="btn-movie__first" type='submit'>Let's watch some movie!</button>
          </div>`;
 
   refs.contStart.insertAdjacentHTML('beforeend', markUpSearchForm);
 
   refs.btnStart.classList.replace('btn-start', 'btn-hidden');
   refs.btnLevelFirstPicture = document.querySelector('#btnLvlFirstPct');
-  console.log(refs.btnLevelFirstPicture);
+  refs.btnMovieFirst = document.querySelector('.btn-movie__first');
+
   refs.btnLevelFirstPicture.addEventListener(
     'click',
     renderSearchFormLevelSecond
@@ -32,5 +34,6 @@ function renderSearchFormLevelSecond(evt) {
         </form>`;
 
   refs.contStart.insertAdjacentHTML('beforeend', markUpSearchFormFirst);
-  console.log(evt);
+  refs.btnLevelFirstPicture.classList.replace('btn__first', 'btn-hidden');
+  refs.btnMovieFirst.classList.replace('btn-movie__first', 'btn-hidden');
 }
